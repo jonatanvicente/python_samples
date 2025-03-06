a5de1b4b7ffc8e84f95306c4ddc489d7285fb11a
Jonatan Vicente <it@jonatanvicente.com>
2025-03-06 12:03:59 +0100

    A bit explanations...

--- tdd/RecordsByDate.py
+++ tdd/RecordsByDate.py
@@ -4,10 +4,12 @@ from dateutil.parser import parse as parse_datetime
 
 class RecordsByDate:
     def __init__(self, data):
+        print('**** Constructor')
         self.data = data
 
     @classmethod
     def read_csv(cls, path):
+        print('**** Method read_csv')
         data = {}
         with open(path) as handle:
             for row in csv.DictReader(handle):
@@ -15,5 +17,10 @@ class RecordsByDate:
                 data[when] = row
         return cls(data)
 
+    # This method allows instances of the class to use the bracket notation (e.g., records[date])
+    # to access data stored in the data attribute.It retrieves the value associated with the given
+    # when key from the data dictionary.
+
     def __getitem__(self, when):
+        print('**** Method __getitem__')
         return self.data[when]
